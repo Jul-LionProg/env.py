dd9dc4694278f2ded91a13a3485fc299dc9eac89
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-16 12:32:34 -0300

    Update env.py

--- env.py
+++ env.py
@@ -6,6 +6,7 @@ MAX_BYTES = 65535
 def servidor(porta):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Cria um socket simples 
     sock.bind(('127.0.0.1', porta)) # Solicita um endereço de rede UDP
-        print('Servidor >> Escutando no IP e porta {}'.format(sock.getsockname()))
+    print('Servidor >> Escutando no IP e porta {}'.format(sock.getsockname()))
     while True: # Executa repetidamente recvfrom()
+    text = data.decode('ascii')
         
